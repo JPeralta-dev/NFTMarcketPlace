a79c8499a4673b292eeb09808408119a3212a499
david123456858 <juandavidperaltafuentes@gmail.com>
2026-05-21 21:37:54 -0500

    refactor: mejora la organización del código y corrige la declaración de eventos

--- src/NTFMarcketPlace.sol
+++ src/NTFMarcketPlace.sol
@@ -2,8 +2,8 @@
 
 pragma solidity 0.8.28;
 
-import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
-import "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import {Ownable} from "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+import {IERC721} from "../lib/openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
 
 error NotPermitValue();
 
@@ -17,18 +17,8 @@ contract NFTMarketPlaceMultiCollection is Ownable {
 
     mapping(address => mapping(uint256 => Listing)) listings;
 
-    event addListingNft(
-        address indexed nftAdress,
-        address indexed seller,
-        uint16 tokenId,
-        uint256 price
-    );
-
-    event cancelledNFT(
-        address indexed nftAdress,
-        address indexed selle,
-        uint16 tokenId
-    );
+    event addListingNft(address indexed nftAdress, address indexed seller, uint16 tokenId, uint256 price);
+    event cancelledNFT(address indexed nftAdress, address indexed selle, uint16 tokenId);
     constructor() Ownable(msg.sender) {}
 
     modifier checkValue(uint256 value) {
@@ -36,28 +26,19 @@ contract NFTMarketPlaceMultiCollection is Ownable {
         _;
     }
 
-    function _checkValue(uint256 value) internal {
-        if (value <= 0) revert NotPermitValue()
+    function _checkValue(uint256 value) internal pure {
+        if (value <= 0) revert NotPermitValue();
     }
 
     // List NFT
 
-    function listNft(
-        address nftAdress_,
-        uint16 tokenId_,
-        uint256 price_
-    ) external checkValue(price_) {
+    function listNft(address nftAdress_, uint16 tokenId_, uint256 price_) external checkValue(price_) {
         // necesitamos evaluar que si sea un token como hacemos eso
         address owner_ = IERC721(nftAdress_).ownerOf(tokenId_);
 
         require(owner_ == msg.sender, "You are not the owner of the NFT");
 
-        Listing memory listin = Listing({
-            seller: msg.sender,
-            nftAddress: nftAdress_,
-            tokenId: tokenId_,
-            price: price_
-        });
+        Listing memory listin = Listing({seller: msg.sender, nftAddress: nftAdress_, tokenId: tokenId_, price: price_});
 
         listings[nftAdress_][tokenId_] = listin;
         emit addListingNft(nftAdress_, msg.sender, tokenId_, price_);
